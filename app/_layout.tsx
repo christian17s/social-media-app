cb65a678bc55b0103bac05a596cdcc8178aebb86
christian <christian17silveira@gmail.com>
2025-03-10 05:30:09 -0300

    tabs and styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from 'expo-router';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Layout() {
-    return <Stack />
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+        <Stack screenOptions={{ headerShown: false}} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
